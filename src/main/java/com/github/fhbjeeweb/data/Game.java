095938d5c9fa5b982552e1c431fcb29dfefcae3c
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-06-13 11:36:09 +0200

    Rename game property name to title

--- src/main/java/com/github/fhbjeeweb/data/Game.java
+++ src/main/java/com/github/fhbjeeweb/data/Game.java
@@ -1,25 +1,25 @@
 package com.github.fhbjeeweb.data;
 
 public class Game {
-    private String name;
+    private String title;
     private String publisher;
     // TODO: Accept a list of genres per game
     private String genre;
 
-    public Game(String name, String publisher, String genre) {
-        this.name = name;
+    public Game(String title, String publisher, String genre) {
+        this.title = title;
         this.publisher = publisher;
         this.genre = genre;
     }
 
     public Game() {};
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getPublisher() {
