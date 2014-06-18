913750499ab1e2fefda9c9e13ffc00a42e102911
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-06-18 18:41:13 +0200

    Add functionality to add a game to the list

--- src/main/java/com/github/fhbjeeweb/manager/MockGameCollectionManager.java
+++ src/main/java/com/github/fhbjeeweb/manager/MockGameCollectionManager.java
@@ -11,6 +11,10 @@ import java.util.List;
 @SessionScoped
 public class MockGameCollectionManager implements Manager {
     private List<Game> games;
+    // Properties used by addGame() action when a new game is added to the list
+    private String gameTitle;
+    private String gamePublisher;
+    private String gameGenres;
 
     public MockGameCollectionManager() {
         games = generateExampleEntries();
@@ -22,6 +26,46 @@ public class MockGameCollectionManager implements Manager {
 
     public void setGames(List<Game> games) {
         this.games = games;
+    }
+
+    public String getGameTitle() {
+        return gameTitle;
+    }
+
+    public void setGameTitle(String gameTitle) {
+        this.gameTitle = gameTitle;
+    }
+
+    public String getGamePublisher() {
+        return gamePublisher;
+    }
+
+    public void setGamePublisher(String gamePublisher) {
+        this.gamePublisher = gamePublisher;
+    }
+
+    public String getGameGenres() {
+        return gameGenres;
+    }
+
+    public void setGameGenres(String gameGenres) {
+        this.gameGenres = gameGenres;
+    }
+
+    // JSF Actions
+    // FIXME: Do not add duplicate entries to the list
+    public String addGame() {
+        Game game = new Game(gameTitle, gamePublisher, gameGenres);
+        games.add(game);
+        return "/listGames";
+    }
+
+    public String cancel() {
+        return "/listGames";
+    }
+
+    public String navigateToAddGame() {
+        return "/addGame";
     }
 
     private List<Game> generateExampleEntries() {
