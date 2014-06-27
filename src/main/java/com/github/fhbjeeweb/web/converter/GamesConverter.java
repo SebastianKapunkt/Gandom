c9f4d1b8b12e53ce90aa9d63c7a7beb2b35fba02
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-06-27 08:08:30 +0200

    Reformat converter classes (mostly Tab => Spaces conversion)

--- src/main/java/com/github/fhbjeeweb/web/converter/GamesConverter.java
+++ src/main/java/com/github/fhbjeeweb/web/converter/GamesConverter.java
@@ -2,59 +2,58 @@ package com.github.fhbjeeweb.web.converter;
 
 import com.github.fhbjeeweb.data.Game;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 @FacesConverter(value = "com.github.fhbjeeweb.GamesConverter")
 public class GamesConverter implements Converter {
 
-	@Override
-	public Object getAsObject(FacesContext facesContext,
-			UIComponent uiComponent, String s) {
-		Set<Game> games = new HashSet<>();
+    @Override
+    public Object getAsObject(FacesContext facesContext,
+                              UIComponent uiComponent, String s) {
+        Set<Game> games = new HashSet<>();
 
-		if (s != null && !s.isEmpty()) {
-			String[] gameNames = s.split(",");
-			for (String name : gameNames) {
-				games.add(new Game(name.trim()));
-			}
-		}
-		return games;
-	}
+        if (s != null && !s.isEmpty()) {
+            String[] gameNames = s.split(",");
+            for (String name : gameNames) {
+                games.add(new Game(name.trim()));
+            }
+        }
+        return games;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public String getAsString(FacesContext facesContext,
-			UIComponent uiComponent, Object o) {
-		Set<Game> gameSet = new TreeSet<>();
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getAsString(FacesContext facesContext,
+                              UIComponent uiComponent, Object o) {
+        Set<Game> gameSet = new TreeSet<>();
 
-		try {
-			gameSet.addAll((Set<Game>) o);
-		} catch (ClassCastException cce) {
-			throw new ConverterException(
-					"Not a list of Games, could not convert to a String");
-		}
+        try {
+            gameSet.addAll((Set<Game>) o);
+        } catch (ClassCastException cce) {
+            throw new ConverterException(
+                    "Not a list of Games, could not convert to a String");
+        }
 
-		StringBuilder gamesStringBuilder = new StringBuilder();
+        StringBuilder gamesStringBuilder = new StringBuilder();
 
-		for (Game game : gameSet) {
-			gamesStringBuilder.append(game.getName()).append(", ");
-		}
+        for (Game game : gameSet) {
+            gamesStringBuilder.append(game.getName()).append(", ");
+        }
 
         String games = gamesStringBuilder.toString();
 
         // Remove trailing comma + whitespace of last append operation
         if (games.length() > 2) {
-            games= games.substring(0, games.length() - 2);
+            games = games.substring(0, games.length() - 2);
         }
 
         return games;
-	}
+    }
 }
--- src/main/java/com/github/fhbjeeweb/web/converter/GenresConverter.java
+++ src/main/java/com/github/fhbjeeweb/web/converter/GenresConverter.java
@@ -7,7 +7,6 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
@@ -15,46 +14,46 @@ import java.util.TreeSet;
 @FacesConverter(value = "com.github.fhbjeeweb.GenresConverter")
 public class GenresConverter implements Converter {
 
-	@Override
-	public Object getAsObject(FacesContext facesContext,
-			UIComponent uiComponent, String s) {
-		Set<Genre> genres = new HashSet<>();
+    @Override
+    public Object getAsObject(FacesContext facesContext,
+                              UIComponent uiComponent, String s) {
+        Set<Genre> genres = new HashSet<>();
 
-		if (s != null && !s.isEmpty()) {
-			String[] genreNames = s.split(",");
-			for (String name : genreNames) {
-				genres.add(new Genre(name.trim()));
-			}
-		}
-		return genres;
-	}
+        if (s != null && !s.isEmpty()) {
+            String[] genreNames = s.split(",");
+            for (String name : genreNames) {
+                genres.add(new Genre(name.trim()));
+            }
+        }
+        return genres;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public String getAsString(FacesContext facesContext,
-			UIComponent uiComponent, Object o) {
-		Set<Genre> genreSet = new TreeSet<>();
+    @Override
+    @SuppressWarnings("unchecked")
+    public String getAsString(FacesContext facesContext,
+                              UIComponent uiComponent, Object o) {
+        Set<Genre> genreSet = new TreeSet<>();
 
-		try {
-			genreSet.addAll((Set<Genre>) o);
-		} catch (ClassCastException cce) {
-			throw new ConverterException(
-					"Not a list of Genres. could not convert to a String");
-		}
+        try {
+            genreSet.addAll((Set<Genre>) o);
+        } catch (ClassCastException cce) {
+            throw new ConverterException(
+                    "Not a list of Genres. could not convert to a String");
+        }
 
-		StringBuilder genresStringBuilder = new StringBuilder();
+        StringBuilder genresStringBuilder = new StringBuilder();
 
-		for (Genre genre : genreSet) {
-			genresStringBuilder.append(genre.getName()).append(", ");
-		}
+        for (Genre genre : genreSet) {
+            genresStringBuilder.append(genre.getName()).append(", ");
+        }
 
         String genres = genresStringBuilder.toString();
 
-		// Remove trailing comma + whitespace of last append operation
+        // Remove trailing comma + whitespace of last append operation
         if (genres.length() > 2) {
-            genres= genres.substring(0, genres.length() - 2);
+            genres = genres.substring(0, genres.length() - 2);
         }
 
         return genres;
-	}
+    }
 }
