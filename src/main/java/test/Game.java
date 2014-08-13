68159c1603a8f6ed6d1282ff3d4d1feffcd296b1
Visores <pewpeweule@gmail.com>
2014-08-13 14:47:42 +0200

    Classes for the Games of a User

---
+++ src/main/java/test/Game.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Game {
+
+	private Number appid;
+	private Number playtime_forever;
+	private Number playtime_2weeks;
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this,
+				ToStringStyle.MULTI_LINE_STYLE);
+	}
+	
+	public Number getPlaytime_2weeks() {
+		return playtime_2weeks;
+	}
+
+	public void setPlaytime_2weeks(Number playtime_2weeks) {
+		this.playtime_2weeks = playtime_2weeks;
+	}
+
+	public Number getAppid() {
+		return appid;
+	}
+
+	public void setAppid(Number appid) {
+		this.appid = appid;
+	}
+
+	public Number getPlaytime_forever() {
+		return playtime_forever;
+	}
+
+	public void setPlaytime_forever(Number playtime_forever) {
+		this.playtime_forever = playtime_forever;
+	}
+}
