37ddd726cfb1f4614db8653ff2ffdedb05469a72
Visores <pewpeweule@gmail.com>
2014-08-13 14:47:51 +0200

    Classes for the friends of a user

---
+++ src/main/java/test/Friend.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Friend {
+
+	private Number friend_since;
+	private String relationship;
+	private String steamid;
+
+	public Number getFriend_since() {
+		return this.friend_since;
+	}
+
+	public void setFriend_since(Number friend_since) {
+		this.friend_since = friend_since;
+	}
+
+	public String getRelationship() {
+		return this.relationship;
+	}
+
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+
+	public String getSteamid() {
+		return this.steamid;
+	}
+
+	public void setSteamid(String steamid) {
+		this.steamid = steamid;
+	}
+}
---
+++ src/main/java/test/Friends.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Set;
+
+public class Friends {
+
+	private Set<Friend> friends;
+
+	public Set<Friend> getFriends() {
+		return this.friends;
+	}
+
+	public void setFriends(Set<Friend> friends) {
+		this.friends = friends;
+	}
+}
