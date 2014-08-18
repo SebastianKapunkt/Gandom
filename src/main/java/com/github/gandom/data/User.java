209749e255ada020c0f7e78c09ded00f50e7b73b
Visores <pewpeweule@gmail.com>
2014-08-18 13:48:22 +0200

    User to collect all data in one.

---
+++ src/main/java/com/github/gandom/data/User.java
@@ -0,0 +1,12 @@
+package com.github.gandom.data;
+
+import java.util.Set;
+
+public class User {
+
+	private String steamId;
+	private Player player;
+	private Set<Game> games;
+	private Set<Friend> friendlist;
+
+}
