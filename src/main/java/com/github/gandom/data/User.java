8bb2cfcb26a8d301cdf802f4b6928f7a7ca00aeb
Visores <pewpeweule@gmail.com>
2014-08-18 15:40:20 +0200

    Class User connects all data that can get collect about a person

--- src/main/java/com/github/gandom/data/User.java
+++ src/main/java/com/github/gandom/data/User.java
@@ -2,11 +2,50 @@ package com.github.gandom.data;
 
 import java.util.Set;
 
-public class User {
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
+public class User {
 	private String steamId;
 	private Player player;
 	private Set<Game> games;
 	private Set<Friend> friendlist;
 
+	public String getSteamId() {
+		return steamId;
+	}
+
+	public void setSteamId(String steamId) {
+		this.steamId = steamId;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public Set<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(Set<Game> games) {
+		this.games = games;
+	}
+
+	public Set<Friend> getFriendlist() {
+		return friendlist;
+	}
+
+	public void setFriendlist(Set<Friend> friendlist) {
+		this.friendlist = friendlist;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this,
+				ToStringStyle.MULTI_LINE_STYLE);
+	}
 }
