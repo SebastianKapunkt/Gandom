80db1d185b9623b24c740511c7ee9c2395f87021
Visores <pewpeweule@gmail.com>
2014-08-21 17:50:07 +0200

    a bit optimisation

--- src/main/java/com/github/gandom/manager/GameIdentifier.java
+++ src/main/java/com/github/gandom/manager/GameIdentifier.java
@@ -25,7 +25,6 @@ public class GameIdentifier {
 	public Set<Data> resolveGames(Set<Game> games) throws JSONException,
 			IOException {
 		Set<Data> resolvedGames = new HashSet<Data>();
-		JSONObject storeobject = new JSONObject();
 		Set<Game> split = new HashSet<Game>();
 		int i = 0;
 
@@ -33,29 +32,26 @@ public class GameIdentifier {
 			split.add(game);
 			i++;
 			if (i % 20 == 0) {
-				storeobject = sendRequest(split);
-				resolvedGames.addAll(convert(storeobject));
+				resolvedGames.addAll(convertToPojo(sendRequest(split)));
 				split.clear();
 			}
 		}
 
-		storeobject = sendRequest(split);
-		resolvedGames.addAll(convert(storeobject));
+		resolvedGames.addAll(convertToPojo(sendRequest(split)));
 
 		return resolvedGames;
 	}
 
-	private Collection<Data> convert(JSONObject storeobject)
+	private Collection<Data> convertToPojo(JSONObject storeobject)
 			throws JsonParseException, JsonMappingException, IOException {
 		Set<Data> bundle = new HashSet<Data>();
-		JsonConverter converter = new JsonConverter();
 		ObjectMapper mapper = new ObjectMapper();
 		JSONObject zw = new JSONObject();
 
 		for (Object object : storeobject.keySet()) {
 			zw = (JSONObject) storeobject.get(object.toString());
 			if (zw.get("success").toString() == "true") {
-				JsonNode node = converter.convertJsonFormat((JSONObject) zw
+				JsonNode node = JsonConverter.convertJsonFormat((JSONObject) zw
 						.get("data"));
 				bundle.add(mapper.readValue(new TreeTraversingParser(node),
 						Data.class));
@@ -74,7 +70,7 @@ public class GameIdentifier {
 		}
 
 		System.out.println(appIds);
-		
+
 		return JsonReader.readJsonFromUrl(appIds.toString());
 	}
 }
