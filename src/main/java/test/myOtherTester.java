a2f8d8f99bbe69cfcc6fc38fa0b72b3d79d5fee0
Visores <pewpeweule@gmail.com>
2014-09-10 20:02:36 +0200

    just shift+F stuff and test drivers

--- src/main/java/test/myOtherTester.java
+++ src/main/java/test/myOtherTester.java
@@ -1,43 +1,45 @@
 package test;
 
-import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.TreeTraversingParser;
-import com.github.gandom.json.JsonConverter;
-import com.github.gandom.json.JsonReader;
-import com.github.gandom.steam.game.Data;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class myOtherTester {
+    public static void main(String args[]) throws InterruptedException, ExecutionException {
 
-	public static void main(String[] args) {
-		JSONObject storeobject = new JSONObject();
-		try {
-			storeobject = JsonReader.readJsonFromUrl("http://store.steampowered.com/api/appdetails?appids=440");
-		} catch (JSONException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Set keyset = new HashSet();
-		keyset = storeobject.keySet();
-		
-		for (Object object : keyset) {
-			storeobject = (JSONObject) storeobject.get(object.toString());
-			JsonConverter converter = new JsonConverter();
-			JsonNode jsonNode = converter.convertJsonFormat((JSONObject) storeobject.get("data"));
-			ObjectMapper mapper = new ObjectMapper();
-			try {
-				Data data = mapper.readValue(new TreeTraversingParser(jsonNode), Data.class);
-				System.out.println(object+" "+data.getHeader_image());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    	ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    	Set<Callable<String>> callables = new HashSet<Callable<String>>();
+
+    	callables.add(new Callable<String>() {
+    	    public String call() throws Exception {
+    	        return "Task 1";
+    	    }
+    	});
+    	callables.add(new Callable<String>() {
+    	    public String call() throws Exception {
+    	        return "Task 2";
+    	    }
+    	});
+    	callables.add(new Callable<String>() {
+    	    public String call() throws Exception {
+    	        return "Task 3";
+    	    }
+    	});
+
+    	System.out.println("hallo");
+    	
+    	List<Future<String>> futures = executorService.invokeAll(callables);
+
+    	for(Future<String> future : futures){
+    	    System.out.println("future.get = " + future.get());
+    	}
+
+    	executorService.shutdown();
+    }
 }
--- src/main/java/test/myTester.java
+++ src/main/java/test/myTester.java
@@ -3,19 +3,21 @@ package test;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import org.json.JSONException;
 
+import com.github.gandom.manager.GameIdentiferMT;
 import com.github.gandom.manager.GameIdentifier;
 import com.github.gandom.manager.GenerateUser;
 import com.github.gandom.manager.UserOperations;
+import com.github.gandom.steam.game.Categories;
 import com.github.gandom.steam.game.Data;
 import com.github.gandom.data.Game;
 import com.github.gandom.data.User;
 
 public class myTester {
 	public static void main(String[] args) {
-		long start = System.nanoTime();
 
 		// 3k games: 76561197987370777
 		// meine: 76561198034249290
@@ -23,9 +25,9 @@ public class myTester {
 		// Sisores: 76561197983850468
 		// Daniel: 76561198141599131
 
-		String steamId = "76561198141599131";
+		String steamId = "76561198034249290";
 		System.out.println("\n--##-- Generating UserOne --##--");
-		
+
 		User userOne = GenerateUser.User(steamId);
 
 		System.out.println("\n--##-- Generating UserTwo --##--");
@@ -59,25 +61,39 @@ public class myTester {
 		for (Game game : userTwo.getGames()) {
 			System.out.print(game.getAppid().toString() + "\t");
 		}
-		
+
 		System.out.println("\n");
 		System.out.println("--##-- resolve games --##--");
 
+		long start = System.nanoTime();
+		
 		Set<Data> bla = new HashSet<Data>();
 		try {
-			bla = GameIdentifier.resolveGames(same);
+			bla = GameIdentiferMT.resolveGames(userOne.getGames());
 		} catch (JSONException | IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
 			e.printStackTrace();
 		}
 
 		System.out.println("\n--##-- Listing resolved Games --##--");
 		for (Data data : bla) {
+			System.out.print(data.getSteam_appid() + " # ");
 			System.out.print(data.getName() + " ### ");
-			System.out.println(data.getCategories());
+			if (data.getCategories() != null) {
+				for (Categories cat : data.getCategories()) {
+					if (!(cat.getDescription() == "")) {
+						System.out.print(cat.getDescription() + " ");
+					}
+				}
+			}
+			System.out.println();
 		}
 
 		long end = System.nanoTime();
 		long elapsedTime = end - start;
-		System.out.println("\n"+(double) elapsedTime / 1000000000.0+"sec");
+		System.out.println("\n" + (double) elapsedTime / 1000000000.0 + "sec");
 	}
 }
