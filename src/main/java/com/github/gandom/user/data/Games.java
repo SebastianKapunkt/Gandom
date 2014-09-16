7e5bfa061cf990295c32f5e75e22a5eccc3d9b09
Visores <pewpeweule@gmail.com>
2014-09-16 20:32:26 +0200

    Game and Games Class
    adding new Classes, representing the Json Objects that get beck by
    Steam Web API : OwnedGames

---
+++ src/main/java/com/github/gandom/user/data/Games.java
@@ -0,0 +1,27 @@
+package com.github.gandom.user.data;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Games {
+	private Number game_count;
+	private List<Game> games;
+
+	public Number getGame_count() {
+		return this.game_count;
+	}
+
+	public void setGame_count(Number game_count) {
+		this.game_count = game_count;
+	}
+
+	public List<Game> getGames() {
+		return this.games;
+	}
+
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+}
