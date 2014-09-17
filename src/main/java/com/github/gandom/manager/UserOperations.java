6bd5c9ec412fb8f97449b5ee7301b864ce98c46d
Visores <pewpeweule@gmail.com>
2014-09-17 10:16:23 +0200

    Edit UserOperations
    Had to change some because of restructuring

--- src/main/java/com/github/gandom/manager/UserOperations.java
+++ src/main/java/com/github/gandom/manager/UserOperations.java
@@ -3,21 +3,21 @@ package com.github.gandom.manager;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.gandom.data.Game;
-import com.github.gandom.data.User;
+import com.github.gandom.user.data.Game;
+import com.github.gandom.user.data.Games;
 
 public class UserOperations {
 
-	public static Set<Game> equalCompareGameList(User userOne, User userTwo) {
+	public static Set<Game> equalCompareGameList(Games userOne, Games userTwo) {
 		Set<Game> one = new HashSet<Game>();
 		one.addAll(userOne.getGames());
-
+		
 		one.retainAll(userTwo.getGames());
 
 		return one;
 	}
 
-	public static Set<Game> unequalComparedGameList(User userOne, User userTwo) {
+	public static Set<Game> unequalComparedGameList(Games userOne, Games userTwo) {
 		Set<Game> one = new HashSet<Game>();
 		Set<Game> two = new HashSet<Game>();
 
