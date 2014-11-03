cf078672531ea44ff1063e1b6cc2a8e2b16bc005
Visores <pewpeweule@gmail.com>
2014-11-03 21:36:02 +0100

    Edit Class GameIdentifier
    Steam removed multiple aphids in url —> changed code for that.
    Removed/Cleaned up some code

--- src/main/java/com/github/gandom/manager/GameIdentifier.java
+++ src/main/java/com/github/gandom/manager/GameIdentifier.java
@@ -23,33 +23,19 @@ import com.github.gandom.user.data.Game;
 
 public class GameIdentifier {
 
-	private static String url = "http://store.steampowered.com/api/appdetails?appids=";
-
 	public static Set<Data> resolveGames(Set<Game> games)
-			throws InterruptedException, JsonParseException,
-			JsonMappingException, IOException, ExecutionException {
-		Set<Data> resolvedGames = new HashSet<Data>();
-		Set<Game> split = new HashSet<Game>();
-		Set<String> holder = new HashSet<String>();
-		StringBuilder appIds = new StringBuilder();
-		int i = 0;
+			throws InterruptedException, IOException, ExecutionException {
+		Set<Data> resolvedGames = new HashSet<>();
+		Set<String> holder = new HashSet<>();
 
 		for (Game game : games) {
-			split.add(game);
-			i++;
-			appIds.append(game.getAppid() + ",");
-			if (i % 10 == 0) {
-				holder.add(url + appIds.toString());
-				appIds.setLength(0);
-			}
+			holder.add("http://store.steampowered.com/api/appdetails?appids=" + game.getAppid());
 		}
 
-		holder.add(url + appIds.toString());
-
-		int threadNumber = 5;
+		int threadNumber = 10;
 
 		ExecutorService executor = Executors.newFixedThreadPool(threadNumber);
-		List<Future<JSONObject>> list = new ArrayList<Future<JSONObject>>();
+		List<Future<JSONObject>> list = new ArrayList<>();
 
 		for (String string : holder) {
 			Callable<JSONObject> worker = new CallableJsonReader(string);
@@ -67,13 +53,13 @@ public class GameIdentifier {
 	}
 
 	private static Collection<Data> convertToPojo(JSONObject storeobject)
-			throws JsonParseException, JsonMappingException, IOException {
-		Set<Data> bundle = new HashSet<Data>();
-		JSONObject zw = new JSONObject();
+			throws IOException {
+		Set<Data> bundle = new HashSet<>();
+		JSONObject zw;
 
 		for (Object object : storeobject.keySet()) {
 			zw = (JSONObject) storeobject.get(object.toString());
-			if (zw.get("success").toString() == "true") {
+			if (zw.get("success").toString().equals("true")) {
 				bundle.add((Data) JsonToPojo.mapOnPojo(
 						(JSONObject) zw.get("data"), new Data()));
 			}
