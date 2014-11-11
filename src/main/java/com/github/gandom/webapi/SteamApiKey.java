f262f047fe4ac2f14dedeb7d48b89fe156219091
Visores <kindt@fh-brandenburg.de>
2014-11-11 19:45:44 +0100

    Edit SteamApiKey, the path now get set, depending on OS

--- src/main/java/com/github/gandom/webapi/SteamApiKey.java
+++ src/main/java/com/github/gandom/webapi/SteamApiKey.java
@@ -6,17 +6,37 @@ import java.io.IOException;
 
 public class SteamApiKey {
 	private static String steamApiKey;
+	private static String OS = null;
+	private static String path;
 
 	@SuppressWarnings("resource")
 	public static String getSteamApiKey() {
+
+		if (isWindows()) {
+			path = "C:/Users/Visores/Google Drive/Privat/mySteamAPIKey.txt";
+		} else {
+			path = "/Users/TheDeveloper/Development/mySteamAPIKey.txt";
+		}
+		BufferedReader reader;
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(
-					"/Users/TheDeveloper/Development/mySteamAPIKey.txt"));
-			// Windows C:/Users/Visores/Google Drive/Privat/mySteamAPIKey.txt
+			reader = new BufferedReader(new FileReader(path));
 			steamApiKey = reader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 		return steamApiKey;
 	}
+
+	public static String getOsName() {
+		if (OS == null) {
+			OS = System.getProperty("os.name");
+		}
+		return OS;
+	}
+
+	public static boolean isWindows() {
+		return getOsName().startsWith("Windows");
+	}
+	
 }
